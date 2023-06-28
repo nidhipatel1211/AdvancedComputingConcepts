076d81a658e1c1504692274188cd66691ff98194
Nidhi Patel <85288488+nidhipatel1211@users.noreply.github.com>
2023-06-28 17:24:29 -0400

    Create MaxHeap.java

---
+++ src/Assignment1/DataStructure/MaxHeap.java
@@ -0,0 +1,18 @@
+package Assignment1.DataStructure;
+
+import java.util.ArrayList;
+
+public class MaxHeap {
+	private final ArrayList<Node> nodes;
+	
+	public MaxHeap() {
+		this.nodes = new ArrayList<Node>();
+	}
+	public 
+	public class Node {
+		private final Integer value;
+		private final String tag;
+		
+		
+	}
+}
